b4f52e14844c17b635367baf1a9b1482f9a01cb9
Alexander Kirilin <nerosashok@mail.ru>
2023-01-22 12:13:13 +0300

    Решены задачи, заданные на дом на 6 семинаре C#

---
+++ 41/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArray(int count)                                    //создание массива заданной длины и его заполнение значениями, вручную пользователем
+{
+    int i = 0;
+    int[] result = new int[count];
+    while (i < count)
+    {
+        System.Console.WriteLine($"Введите {i + 1}-е значение массива");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+        i = i + 1;
+    }
+    return result;
+}
+void PrintArray(int[] array)                                    //метод выведения значений на экран
+{
+    Console.WriteLine($"[{String.Join(", ", array)}]");
+}
+int CountPositiveArrayElements(int[] array)                     //метод подсчета положительных значений в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 0) count = count + 1;
+    }
+    return count;
+}
+Console.Clear();
+System.Console.WriteLine("Введите кол-во элементов, которые будут представлены в массиве");
+int numElements = Convert.ToInt32(Console.ReadLine());
+if (numElements > 1)
+{
+int[] MyArray = CreateArray(numElements);
+PrintArray(MyArray);
+int PosNumbers = CountPositiveArrayElements(MyArray);
+System.Console.WriteLine($"{PosNumbers} - кол-во элементов, в заданном массиве, имеют положительные значения");
+}
+else
+{
+System.Console.WriteLine("Ошибка! Введенно некорректное значение для формирования массива");    
+}
---
+++ 43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+void LineIntersectionCheck(double firstK, double firstB, double secondK, double secondB)
+{
+    // k1x+b1=k2x+b2
+    // k1x-k2x = b2-b1
+    // x(k1-k2) = b2-b1
+    // x = (b2-b1) / (k1-k2)
+    // y = k1 * ((b2-b1) / (k1-k2)) + b1
+
+    double x = (secondB - firstB) / (firstK - secondK);
+    double y = firstK * ((secondB - firstB) / (firstK - secondK)) + firstB;
+    System.Console.WriteLine($"Координаты пересечения прямых: x = {x}; y = {y};");
+}
+Console.Clear();
+System.Console.WriteLine("Даны уравнения: y = k1 * x + b1, y = k2 * x + b2");
+System.Console.WriteLine("Введите значения k1, b1, k2, b2 для нахождения точки пересечания прямых на графике");
+System.Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2 && b1 == b2) System.Console.WriteLine("Все точки заданных прямых совпадают. Одна прямая лежит на другой.");
+if (k1 == k2 && b1 != b2) System.Console.WriteLine("Точки пересечения прямых отсутствуют. Прямые, заданные формулами, параллельны и не пересекаются на плоскости");
+if (k1 != k2) LineIntersectionCheck(k1, b1, k2, b2);
